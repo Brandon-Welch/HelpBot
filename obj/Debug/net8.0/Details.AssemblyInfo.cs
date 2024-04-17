0ada2156f428fec5ea25d04e1e813ad98fb24fea
Brandon-Welch <bwelch3800@hotmail.com>
2024-04-17 14:01:18 -0400

    updates to bot

--- obj/Debug/net8.0/Details.AssemblyInfo.cs
+++ obj/Debug/net8.0/Details.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Details")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51d3a33438d381768387477507d5280d21d60afb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Details")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Details")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
