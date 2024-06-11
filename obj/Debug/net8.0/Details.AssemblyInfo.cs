21239baa1b85ca06f8e042260e6a0b3ec43f2d03
Brandon-Welch <bwelch3800@hotmail.com>
2024-06-11 08:00:39 -0400

    review material added

--- obj/Debug/net8.0/Details.AssemblyInfo.cs
+++ obj/Debug/net8.0/Details.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Details")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337482dc44fcd0ccee0aaa2306ddd8ee45c8b90b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d31ca37e5316e7e5f6be2717f3991fac1178bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Details")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Details")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
